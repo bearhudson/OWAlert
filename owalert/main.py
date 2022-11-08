4c5af1ba6321961e5cc9b7d817a8336840d57a0b
Brian Hudson <reallybrianhudson@gmail.com>
2022-11-08 12:57:52 -0500

    Starting.

--- owalert/main.py
+++ owalert/main.py
@@ -1,13 +1,23 @@
 from openweatherclass import OpenWeatherClass
 from time import sleep
-import requests
 import os
 
 API_KEY = os.getenv("API_KEY")
 
-while True:
-    weather = OpenWeatherClass(api_key=API_KEY, zipcode='02188', units='imperial')
-    print(weather.geo_data['name'])
-    print(weather.weather_data['current']['temp'])
-    print(weather.check_condition(weather.weather_data['current']['weather'][0]['id']))
-    sleep(1)
+
+class OWAlertClass:
+    def __init__(self, **kwargs):
+        self.owc = OpenWeatherClass(**kwargs)
+
+
+def main():
+    ow_object = OWAlertClass(api_key=API_KEY, zipcode='02188', units='imperial')
+    hourly_weather = ow_object.owc.weather_data['hourly']
+    for hour in hourly_weather:
+        if 'alerts' in hour:
+            print(hour)
+
+
+if __name__ == "__main__":
+    main()
+
