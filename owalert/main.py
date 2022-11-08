13ed318fb663544e2466093bd91893ce2c71ad5d
Brian Hudson <reallybrianhudson@gmail.com>
2022-11-08 14:04:56 -0500

    Check complete

--- owalert/main.py
+++ owalert/main.py
@@ -1,5 +1,6 @@
 from openweatherclass import OpenWeatherClass
 from time import sleep
+from _datetime import datetime
 import os
 
 API_KEY = os.getenv("API_KEY")
@@ -9,15 +10,22 @@ SLEEP = 3600
 class OWAlertClass:
     def __init__(self, **kwargs):
         self.owc = OpenWeatherClass(**kwargs)
+        self.is_alerted: bool = False
+        self.last_alerted: datetime
 
 
 def main():
-    owalert_object = OWAlertClass(api_key=API_KEY, zipcode='02188', units='imperial')
+    owalert_object = OWAlertClass(api_key=API_KEY, zipcode='92065', units='metric')
     while True:
         hourly_weather = owalert_object.owc.weather_data['hourly']
-        for hour in hourly_weather:
+        for hour in hourly_weather[1:2]:
+            for status in hour['weather']:
+                cur_code = int(status['id'])
+                if cur_code <= 622:
+                    print(owalert_object.owc.check_condition(cur_code))
+                    print(datetime.strftime(datetime.fromtimestamp(hour['dt']), '%H:%M'))
             if 'alerts' in hour:
-                print(hour)
+                print(hour['alerts'])
         sleep(SLEEP)
         owalert_object.owc.update_weather()
 
