4348474cc155660d0dde509894c215d5bc6d9aa3
Brian Hudson <reallybrianhudson@gmail.com>
2022-11-11 12:12:51 -0500

    Adding Alert text handling.

--- owalert/main.py
+++ owalert/main.py
@@ -19,36 +19,38 @@ class OWAlertClass:
     def update_expiry(self, expires: int):
         self.expires_dt = datetime.fromtimestamp(expires)
 
-    def get_hash(self) -> str:
-        return self.alert_hash
-
-    def update_hash(self, text):
-        self.alert_hash = str(hash(text))
-
 
 def main():
     owalert = OWAlertClass(api_key=API_KEY, zipcode='27208', units='metric')
     while True:
         hourly_weather = owalert.owc.weather_data['hourly']
         if 'alerts' in owalert.owc.weather_data and owalert.is_alerted is False:
-            cur_code = owalert.owc.weather_data['current']['weather'][0]['id']
-
-            alert_text = f"Insert Text here [{cur_code}]."
+            description_title = owalert.owc.weather_data['alerts'][0]['event']
+            description_txt = owalert.owc.weather_data['alerts'][0]['description']
+            sender_txt = owalert.owc.weather_data['alerts'][0]['sender_name']
+            start_dt = datetime.fromtimestamp(owalert.owc.weather_data['alerts'][0]['start'])
+            owalert.update_expiry(owalert.owc.weather_data['alerts'][0]['end'])
+            alert_text = f"Subject: Alert! {description_title}\n" \
+                         f"Description: From {sender_txt} at {datetime.strftime(owalert.request_dt, '%H:%M')}\n" \
+                         f"Start time: {datetime.strftime(start_dt, '%H:%M')} -- " \
+                         f"End time: {datetime.strftime(owalert.expires_dt, '%H:%M')}\n" \
+                         f"{description_txt}\n"
             send_message(alert_text)
+            owalert.is_alerted = True
         else:
             for hour in hourly_weather[1:2]:
                 for status in hour['weather']:
                     cur_code = int(status['id'])
-                    if cur_code <= 622 and owalert.is_alerted is False:
+                    if cur_code < 800 and owalert.is_alerted is False:
                         precip_text = f"Subject: Precip. for {owalert.owc.zipcode}\n" \
                                       f"Condition: {owalert.owc.check_condition(cur_code)}\n " \
                                       f"Alert at: {datetime.strftime(owalert.request_dt, '%H:%M')}\n " \
                                       f"Expires at: {datetime.strftime(owalert.expires_dt, '%H:%M')}"
                         send_message(precip_text)
-            send_message("No Alerts")
-        print(owalert.request_time)
-        print(owalert.request_dt)
-        print(owalert.expires_dt)
+        # print(owalert.request_time)
+        # print(owalert.request_dt)
+        # print(owalert.expires_dt)
+        # print(owalert.is_alerted)
         sleep(SLEEP)
         owalert.owc.update_weather()
         if owalert.expires_dt < owalert.request_dt:
@@ -57,6 +59,7 @@ def main():
 
 def send_message(msg_text):
     print(f"{msg_text}")
+    # TODO: Add mail notification function
 
 
 if __name__ == "__main__":
